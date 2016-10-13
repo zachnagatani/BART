4c4ec7c6299324989b25c7472a5216163e023f3d
Zachariah Nagatani <Zachariah Nagatani>
2016-10-12 23:53:27 -0700

    Feat: Home Skeleton and styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+var app = angular.module('app', ['ui.router']);
+
+(function(){
+	'use strict';
+
+	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: 'templates/home.html',
+				controller: 'homeCtrl'
+			})
+			.state('planner', {
+				url: '/planner',
+				templateUrl: 'templates/planner.html',
+				controller: 'plannerCtrl'
+			})
+			.state('trip', {
+				url: '/trip',
+				templateUrl: 'templates/trip.html',
+				controller: 'tripCtrl'
+			});
+	}]);
+})();
