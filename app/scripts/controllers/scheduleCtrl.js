3aa8b8dc876de40042e87d0ac6e8a7bd82ee5698
Zachariah Nagatani <Zachariah Nagatani>
2016-10-19 14:04:36 -0700

    Feat: Schedule Templating

--- app/scripts/controllers/scheduleCtrl.js
+++ app/scripts/controllers/scheduleCtrl.js
@@ -2,35 +2,33 @@
 	'use strict';
 
 	app.controller('scheduleCtrl', ['$scope', '$timeout', 'apiCalls', 'xmlToJSON', function($scope, $timeout, apiCalls, xmlToJSON) {
-	  // $(document).ready(function(){
-	  //   $('.materialboxed').materialbox();
-	  // });
-	  
-	  // $(document).ready(function(){
-	  //   $('ul.tabs').tabs();
-	  // });
+		$(document).ready(function(){
+			$('.materialboxed').materialbox();
+		});
 
-		$scope.test = function() {
-			console.log('test');
-		};
+		$(document).ready(function(){
+			$('ul.tabs').tabs();
+		});
 
 		$scope.getSchedule = function(routeNum) {
-		  apiCalls.makeCall('http://api.bart.gov/api/sched.aspx?cmd=routesched&route=1&key=MW9S-E7SL-26DU-VV8V')
-		  	.then(function(response) {
-		  		return xmlToJSON.dataToDoc(response.data);
-		  	})
-		  	.then(function(xml) {
-		  		return xmlToJSON.xmlToJSON(xml);
-		  	})
-		  	.then(function(json) {
-		  		$scope.schedule = json;
-		  	}).then(function() {
-		  		console.log($scope.schedule);
-		  	});
+			$scope.schedule = null;
+			
+			apiCalls.makeCall('http://api.bart.gov/api/sched.aspx?cmd=routesched&route=' + routeNum + '&key=MW9S-E7SL-26DU-VV8V')
+				.then(function(response) {
+					return xmlToJSON.dataToDoc(response.data);
+				})
+				.then(function(xml) {
+					return xmlToJSON.xmlToJSON(xml);
+				})
+				.then(function(json) {
+					$scope.schedule = json;
+				}).then(function() {
+					console.log($scope.schedule);
+				});
+			};
+
+		$scope.hideTables = function() {
+			$('.table--schedule').hide();
 		};
-
-		$scope.getSchedule();
-
-
 	}]);	
 })();
