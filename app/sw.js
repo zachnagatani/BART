4cd5e102eb0f66a7cfeff6ad760fb7be4898c948
Zachariah Nagatani <Zachariah Nagatani>
2016-10-25 14:25:50 -0700

    Feat: Refactor sw.js for Dist

--- app/sw.js
+++ app/sw.js
@@ -8,28 +8,18 @@ this.addEventListener('install', function(event) {
 					'http://localhost:9000/index.html',
 					'http://localhost:9000/sw.js',
 					'http://localhost:9000/styles/main.css',
-					'http://localhost:9000/scripts/idbp.js',
+					'http://localhost:9000/styles/vendor.css',
 					'http://localhost:9000/scripts/main.js',
-					'http://localhost:9000/scripts/services/services.js',
-					'http://localhost:9000/scripts/controllers/indexCtrl.js',
-					'http://localhost:9000/scripts/controllers/homeCtrl.js',
-					'http://localhost:9000/scripts/controllers/plannerCtrl.js',
-					'http://localhost:9000/scripts/controllers/tripCtrl.js',
-					'http://localhost:9000/scripts/controllers/scheduleCtrl.js',
+					'http://localhost:9000/scripts/vendor.js',
 					'http://localhost:9000/templates/home.html',
 					'http://localhost:9000/templates/planner.html',
 					'http://localhost:9000/templates/trip.html',
 					'http://localhost:9000/templates/schedule.html',
 					'http://localhost:9000/images/bridge1.png',
 					'http://localhost:9000/images/bartmap.png',
-					'http://localhost:9000/bower_components/materialize/bin/materialize.css',
-					'http://localhost:9000/bower_components/jquery/dist/jquery.js',
-					'http://localhost:9000/bower_components/angular/angular.js',
-					'http://localhost:9000/bower_components/angular-ui-router/release/angular-ui-router.js',
-					'http://localhost:9000/bower_components/angular-aria/angular-aria.js',
-					'http://localhost:9000/bower_components/angular-animate/angular-animate.js',
-					'http://localhost:9000/bower_components/materialize/bin/materialize.js',
-					'http://localhost:9000/bower_components/materialize/fonts/roboto/Roboto-Regular.woff2'
+					'http://localhost:9000/fonts/roboto/Roboto-Regular.ttf',
+					'http://localhost:9000/fonts/roboto/Roboto-Regular.woff2',
+					'http://localhost:9000/fonts/roboto/Roboto-Regular.woff'
 				])
 			})
 	);
