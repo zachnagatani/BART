73f3682f98ca22bbb8dc3fe79b3bec310c5f2037
Zachariah Nagatani <Zachariah Nagatani>
2016-10-26 10:21:51 -0700

    Fix: Refactor Service Worker v 12

--- app/sw.js
+++ app/sw.js
@@ -1,26 +1,42 @@
-var staticCache = 'static-cache-v5';
+var staticCache = 'static-cache-v12';
 
 this.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCache)
 			.then(function(cache) {
 				return cache.addAll([
-					'http://localhost:9000/index.html',
-					'http://localhost:9000/sw.js',
-					'http://localhost:9000/styles/main.css',
-					'http://localhost:9000/styles/vendor.css',
-					'http://localhost:9000/scripts/main.js',
-					'http://localhost:9000/scripts/vendor.js',
-					'http://localhost:9000/templates/home.html',
-					'http://localhost:9000/templates/planner.html',
-					'http://localhost:9000/templates/trip.html',
-					'http://localhost:9000/templates/schedule.html',
-					'http://localhost:9000/images/bridge1.png',
-					'http://localhost:9000/images/bartmap.png',
-					'http://localhost:9000/fonts/roboto/Roboto-Regular.ttf',
-					'http://localhost:9000/fonts/roboto/Roboto-Regular.woff2',
-					'http://localhost:9000/fonts/roboto/Roboto-Regular.woff'
+					'/',
+					'/styles/vendor.css',
+					'/styles/main.css',
+					'/scripts/vendor.js',
+					'/scripts/main.js',
+					'/templates/home.html',
+					'/templates/planner.html',
+					'/templates/trip.html',
+					'/templates/schedule.html',
+					'/images/bridge1.png',
+					'/images/bartmap.png',
+					'/fonts/roboto/Roboto-Regular.ttf',
+					'/fonts/roboto/Roboto-Regular.woff2',
+					'/fonts/roboto/Roboto-Regular.woff'
 				])
+			})
+	);
+});
+
+this.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys()
+			.then(function(cacheNames) {
+				return Promise.all(
+					cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('static-cache') &&
+						   cacheName !== staticCache;
+					})
+					.map(function(cacheName) {
+						caches.delete(cacheName);
+					})
+				)
 			})
 	);
 });
@@ -41,7 +57,7 @@ function fetchAndCache(myCache, request) {
 this.addEventListener('fetch', function(event) {
 	var requestURL = new URL(event.request.url);
 
-	if(requestURL.href.startsWith('http://localhost:9000/') && !requestURL.href.startsWith('http://localhost:9000/browser-sync/')) {
+	if(requestURL.href.startsWith('/') && !requestURL.href.startsWith('/browser-sync/')) {
 		event.respondWith(fetchAndCache(staticCache, event.request));
 	}
 });
